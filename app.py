566a1a8d7c1dedad8cd2d02b18148beea828a7d2
mahad2810 <164727402+mahad2810@users.noreply.github.com>
2024-12-21 21:35:04 +0530

    c

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ from bson import ObjectId
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.secret_key = "supersecretkey"
 # MongoDB configuration
 app.config["MONGO_URI"] = "mongodb+srv://thesevasetufoundation:QDBxMA83Wsiamvyb@sevasetu.qplys.mongodb.net/Khack?retryWrites=true&w=majority&tls=true"
-app.secret_key = "supersecretkey"
 mongo = PyMongo(app)
 
 # Google OAuth configuration
